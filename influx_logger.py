f35e03fd238a259b971071767d68f9da9b841a3f
RKrauja <reinis@campfiresecurity.dk>
2026-06-22 12:21:47 +0200

    Fix InfluxDB client write mode import

--- influx_logger.py
+++ influx_logger.py
@@ -6,7 +6,8 @@ import sys
 from datetime import datetime, timezone
 
 import paho.mqtt.client as mqtt
-from influxdb_client import InfluxDBClient, Point, SYNCHRONOUS
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 APP_NAME = os.environ.get("APP_NAME", "BioreactorDB4")
